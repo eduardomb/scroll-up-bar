a4a722e15eeee236cd53464f651520eb65a40473
Eduardo Barbosa <eduardomb@ufmg.br>
2014-06-22 23:28:10 -0300

    Update release mock script.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,9 @@
 # Update version on package.json
 # Update version on bower.json
+# Update version on scroll-up-bar.jquery.json
 # Update changelog
 # Build (Can't run before updating package.json to update dist files header comment)
 # Commit 'Release x.x.x'
 # Add tag 'x.x.x'
+# Update branch gh-pages
 # push with tag
